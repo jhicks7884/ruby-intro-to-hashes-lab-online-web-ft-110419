2fc7a034bb2ff94cbafe28865a262cb70e8cdf74
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-09 17:15:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,6 +31,7 @@ def monopoly_with_second_tier
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 end
+
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
